1e52bdd111007f6dc2d90a6c55fa9d14d521ce04
Андрей Замятин <andreizamyat1n@yandex.ru>
2024-04-01 13:49:28 +0500

    Добавление документации и первого варианта самостоятельной программы

--- СМ_1/СМ_1.cpp
+++ СМ_1/СМ_1.cpp
@@ -1,4 +1,52 @@
-﻿/*
+﻿#include <iostream>
+#include <fstream>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+class DataHandler {
+public:
+    virtual void process(std::ifstream& in, std::ofstream& out) = 0;
+};
+
+class WordCounter : public DataHandler {
+public:
+    void process(std::ifstream& in, std::ofstream& out) override {
+        std::string word;
+        int count = 0;
+        while (in >> word) {
+            ++count;
+        }
+        out << "Number of words: " << count << std::endl;
+    }
+};
+
+int main() {
+    std::vector<std::thread> threads;
+    std::mutex mtx;
+
+    // Замените эти имена на имена ваших файлов
+    std::vector<std::string> fileNames = { "file1.txt", "file2.txt", "file3.txt" };
+
+    for (const auto& fileName : fileNames) {
+        std::ifstream in(fileName);
+        std::ofstream out(fileName + ".processed");
+
+        WordCounter counter;
+        threads.push_back(std::thread([&] {
+            counter.process(in, out);
+            }));
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    return 0;
+}
+
+
+/*
 Самостоятельная задача: Параллельная обработка текстовых файлов
 
 Создайте систему на C++, которая читает текст из нескольких файлов, 
